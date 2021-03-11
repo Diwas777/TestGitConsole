8da91d639f82d10f183493622a680f4a446d31ef
Diwas777 <poudel.diwas789@gmail.com>
2021-03-11 20:55:16 +0545

    Addded

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -6,7 +6,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!sdfas d");
             Console.WriteLine("From Branch1");
         }
     }
